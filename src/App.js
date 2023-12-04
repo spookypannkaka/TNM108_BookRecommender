9f4d58c1dac7afa997bdf3e88aa8398fbbcd12d7
Lovisa Svensson <77107017+spookypannkaka@users.noreply.github.com>
2023-12-04 14:06:00 +0100

    frontend typ klar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './OpenBook.css';
+
+import BookList from './components/BookList.js'
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentSearch, setCurrentSearch] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    setCurrentSearch(searchTerm);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="book-container">
+        <div className="page">
+          <h1>The Book Recommender</h1>
+          <h3>The story has it that if the reader writes here about what they wish to read in a book, they will get a recommendation that best fits them!</h3>
+          <h2>I wish to read about...</h2>
+          <textarea name="searchForm" value={searchTerm} onChange={handleSearchChange} />
+          <button type="submit" onClick={handleSearchClick}>Recommend Me!</button>
+        </div>
+        <div className="page">
+          <BookList searchTerm={currentSearch}/>
+        </div>
+        </div>
     </div>
   );
 }
